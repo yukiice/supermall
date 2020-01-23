dd02ac9a2bbabb150112386605b82c1b76fa63ff
yukiice <1076702326@qq.com>
2020-01-23 18:59:06 +0800

    推荐信息完成 day01

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,21 @@
 <template>
   <div id="app">
     <router-view></router-view>
-
-<main-tab-bar></main-tab-bar>
+    <main-tab-bar></main-tab-bar>
   </div>
 </template>
 
 <script>
-import MainTabBar from './components/content/mainTabBar/MainTabBar'
+import MainTabBar from "./components/content/mainTabBar/MainTabBar";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-MainTabBar
+    MainTabBar
   }
-}
+};
 </script>
 
 <style>
-@import 'assets/css/base.css'
+@import "assets/css/base.css";
 </style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,24 +3,36 @@
     <nav-bar class="home-nav">
       <div slot="center">购物街</div>
     </nav-bar>
+    <home-swiper :banners="banners"></home-swiper>
+    <recommend-view :recommends="recommends"></recommend-view>
   </div>
 </template>
 
 <script>
 import NavBar from "components/common/navbar/NavBar";
+
+import HomeSwiper from './childComps/HomeSwiper';
+
+import RecommendView from './childComps/RecommendView';
+
 import { getHomeMultidata } from "network/home";
+
+
+
 
 export default {
   name: "Home",
   props: [""],
   data() {
     return {
-      banners: [],
-      recommends: []
+      banners:[],
+      recommends:[]
     };
   },
   components: {
-    NavBar
+    NavBar,
+    HomeSwiper,
+    RecommendView
   },
   created() {
     //请求多个数据
